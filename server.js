8ba9d6e8762fda4b27a7c6bf0fbb53fe010a96b2
Hardik Aggarwal <hardik@Hardiks-MacBook-Pro.local>
2023-07-25 16:03:49 +0530

    Product API's added

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import morgan from "morgan";
 import { connectDB } from "./config/db.js";
 import authRoute from "./routes/authRoute.js";
 import categoryRoute from "./routes/categoryRoute.js";
+import productRoute from "./routes/productRoutes.js"
 import cors from 'cors';
 
 
@@ -32,6 +33,7 @@ app.use(morgan('dev'));
 //routes
 app.use("/api/v1/auth",authRoute);
 app.use("/api/v1/category",categoryRoute);
+app.use("/api/v1/product",productRoute);
 
 
 
